c9d3598a9a8996637ae744fa4fd35bebbcc62f57
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-03-22 18:54:45 +0100

    Add files via upload

--- TextEditor/Form1.cs
+++ TextEditor/Form1.cs
@@ -8,8 +8,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
-namespace TextEditor
+namespace TextEditor2
 {
     public partial class Form1 : Form
     {
@@ -17,65 +19,111 @@ namespace TextEditor
         {
             InitializeComponent();
         }
-
         string text, path;
-        bool opened;
+        bool opened, bold = false, underline = false, italic = false;
+
+        private void btnUnderline_Click(object sender, EventArgs e)
+        {
+            if (underline == false)
+            {
+                rtxtUnos.Font = new Font(rtxtUnos.Font, FontStyle.Underline);
+                underline = true;
+                if (bold == true || italic == true)
+                {
+                    bold = false;
+                    italic = false;
+                }
+            }
+            else
+            {
+                rtxtUnos.Font = new Font(rtxtUnos.Font, FontStyle.Regular);
+                underline = false;
+            }
+        }
+
+        private void btnItalic_Click(object sender, EventArgs e)
+        {
+            if (italic == false)
+            {
+                rtxtUnos.Font = new Font(rtxtUnos.Font, FontStyle.Italic);
+                italic = true;
+                if (bold == true || underline == true)
+                {
+                    bold= false;
+                    underline = false;
+                }
+            }
+            else
+            {
+                rtxtUnos.Font = new Font(rtxtUnos.Font, FontStyle.Regular);
+                italic = false;
+            }
+
+        }
+
         private void btnOpen_Click(object sender, EventArgs e)
         {
-            
-            
+
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
             ofd.FilterIndex = 2;
             ofd.RestoreDirectory = true;
             if (ofd.ShowDialog() == DialogResult.OK)
             {
+                rtxtUnos.Clear();
                 path = ofd.FileName;
                 StreamReader reader = new StreamReader(path);
                 text = reader.ReadToEnd();
-                txtUnos.AppendText(text);
+                rtxtUnos.AppendText(text);
                 reader.Close();
-                opened = true;
+                
             }
+            opened = true;
+        }
+
+        private void btnBold_Click(object sender, EventArgs e)
+        {
+            if(bold == false)
+            {
+                rtxtUnos.Font = new Font(rtxtUnos.Font, FontStyle.Bold);
+                bold=true;
+                if (italic == true || underline == true)
+                {
+                    italic = false;
+                    underline = false;
+                }
+            }
+            else
+            {
+                rtxtUnos.Font = new Font(rtxtUnos.Font, FontStyle.Regular);
+                bold = false;
+            }
+            
+            
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
             if (opened == true)
             {
+                text = rtxtUnos.Text;
                 StreamWriter writer = new StreamWriter(path);
                 writer.WriteLine(text);
                 writer.Close();
             }
-            else {
+            else
+            {
                 SaveFileDialog save = new SaveFileDialog();
                 save.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
                 if (save.ShowDialog() == DialogResult.OK)
                 {
-                    
+                    text = rtxtUnos.Text;
+                    StreamWriter writer = new StreamWriter(save.FileName);
+                    writer.WriteLine(text);
+                    writer.Close();
+
                 }
-
             }
-        }
-
-        private void btnBold_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnItalic_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnClear_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnUnderline_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
